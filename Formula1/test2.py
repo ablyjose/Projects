f5b9a9df9599cf189443bed72cd615c9e2167d9e
Alby Joseph <albysoy1@gmail.com>
2024-10-30 23:20:04 -0500

    switched test 2 back to Austin quali

--- Formula1/test2.py
+++ Formula1/test2.py
@@ -1,6 +1,7 @@
 import fastf1 as ff1
 from fastf1 import plotting
 from fastf1 import utils
+from fastf1.core import Laps
 
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import figure
@@ -17,7 +18,6 @@ quali.load()
 
 driver_1, driver_2 = 'NOR', 'VER'
 
-
 laps_driver_1 = quali.laps.pick_drivers(driver_1)
 laps_driver_2 = quali.laps.pick_drivers(driver_2)
 
@@ -29,6 +29,10 @@ telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
 
 team_1 = fastest_driver_1['Team']
 team_2 = fastest_driver_2['Team']
+
+driver_1_style = plotting.get_driver_style(driver_1, ['color', 'linestyle'], quali)
+driver_2_style = plotting.get_driver_style(driver_2, ['color', 'linestyle'], quali)
+
 
 delta_time, ref_tel, compare_tel = utils.delta_time(fastest_driver_1, fastest_driver_2)
 
@@ -45,33 +49,33 @@ fig, ax = plt.subplots(7, gridspec_kw={'height_ratios': plot_ratios})
 ax[0].title.set_text(plot_title)
 
 
-ax[0].plot(ref_tel['Distance'], -delta_time)
+ax[0].plot(ref_tel['Distance'], delta_time)
 ax[0].axhline(0)
 ax[0].set(ylabel=f"Gap to {driver_2} (s)")
 
-ax[1].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], label=driver_1, color=plotting.get_team_color(team_1, quali))
-ax[1].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], label=driver_2, color=plotting.get_team_color(team_2, quali))
+ax[1].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], **driver_1_style, label=driver_1)
+ax[1].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], **driver_2_style, label=driver_2)
 ax[1].set(ylabel='Speed')
 ax[1].legend(loc="lower right")
 
-ax[2].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Throttle'], label=driver_1, color=plotting.get_team_color(team_1, quali))
-ax[2].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Throttle'], label=driver_2, color=plotting.get_team_color(team_2, quali))
+ax[2].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Throttle'], **driver_1_style, label=driver_1)
+ax[2].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Throttle'], **driver_2_style, label=driver_2)
 ax[2].set(ylabel='Throttle')
 
-ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Brake'], label=driver_1, color=plotting.get_team_color(team_1, quali))
-ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Brake'], label=driver_2, color=plotting.get_team_color(team_2, quali))
+ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Brake'], **driver_1_style, label=driver_1)
+ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Brake'], **driver_2_style, label=driver_2)
 ax[3].set(ylabel='Brake')
 
-ax[4].plot(telemetry_driver_1['Distance'], telemetry_driver_1['nGear'], label=driver_1, color=plotting.get_team_color(team_1, quali))
-ax[4].plot(telemetry_driver_2['Distance'], telemetry_driver_2['nGear'], label=driver_2, color=plotting.get_team_color(team_2, quali))
+ax[4].plot(telemetry_driver_1['Distance'], telemetry_driver_1['nGear'], **driver_1_style, label=driver_1)
+ax[4].plot(telemetry_driver_2['Distance'], telemetry_driver_2['nGear'], **driver_2_style, label=driver_2)
 ax[4].set(ylabel='Gear')
 
-ax[5].plot(telemetry_driver_1['Distance'], telemetry_driver_1['RPM'], label=driver_1, color=plotting.get_team_color(team_1, quali))
-ax[5].plot(telemetry_driver_2['Distance'], telemetry_driver_2['RPM'], label=driver_2, color=plotting.get_team_color(team_2, quali))
+ax[5].plot(telemetry_driver_1['Distance'], telemetry_driver_1['RPM'], **driver_1_style, label=driver_1)
+ax[5].plot(telemetry_driver_2['Distance'], telemetry_driver_2['RPM'], **driver_2_style, label=driver_2)
 ax[5].set(ylabel='RPM')
 
-ax[6].plot(telemetry_driver_1['Distance'], telemetry_driver_1['DRS'], label=driver_1, color=plotting.get_team_color(team_1, quali))
-ax[6].plot(telemetry_driver_2['Distance'], telemetry_driver_2['DRS'], label=driver_2, color=plotting.get_team_color(team_2, quali))
+ax[6].plot(telemetry_driver_1['Distance'], telemetry_driver_1['DRS'], **driver_1_style, label=driver_1)
+ax[6].plot(telemetry_driver_2['Distance'], telemetry_driver_2['DRS'], **driver_2_style, label=driver_2)
 ax[6].set(ylabel='DRS')
 ax[6].set(xlabel='Lap distance (meters)')
 
