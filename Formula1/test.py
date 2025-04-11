fd8bb57777441e1b872dd5a993570848f20ac3a3
Alby Joseph <albysoy1@gmail.com>
2025-04-11 01:07:51 -0500

    True fastf1 testing ground

--- Formula1/test.py
+++ Formula1/test.py
@@ -1,9 +1,4 @@
-from urllib.request import urlopen
-import json
-import pandas as pd
+import fastf1 as ff1
 
-response = urlopen('https://api.openf1.org/v1/car_data?driver_number=16&session_key=9165&drs>=10')
-data = json.loads(response.read().decode('utf-8'))
-
-df = pd.DataFrame(data)
-print(df)
+event = ff1.get_event(2025, 3)
+print(event.get_session_name('R'))
