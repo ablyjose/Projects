6ad7cb3e119fc3543a7bed00b84aeeee293abcc2
Alby Joseph <albysoy1@gmail.com>
2025-04-22 18:25:23 -0500

    Adding tyre deg file to see if testing laps loads; currently doesn't work with old version of FastF1

---
+++ Formula1/tyreDegComparison.py
@@ -0,0 +1,17 @@
+import fastf1 as ff1
+from fastf1 import plotting
+
+from matplotlib import pyplot as plt
+
+
+ff1.Cache.enable_cache('C:/Users/albys/PortProjects/Formula1/cache')
+
+# year, gp, session = 2025, "Bahrain", 'FP2'
+# fp = ff1.get_session(year, gp, session)
+# fp.load()
+
+testing = ff1.get_testing_session(2024, 1, 1)
+print(testing.api_path)
+testing.load()
+
+print(testing.laps)
