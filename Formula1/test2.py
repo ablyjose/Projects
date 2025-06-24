3b9e62a5d1f97465adff52d33e10c3bd6fde2198
Alby Joseph <albysoy1@gmail.com>
2025-06-24 13:04:45 -0500

    updated test2 to account for driver_style deprecation

--- Formula1/test2.py
+++ Formula1/test2.py
@@ -30,8 +30,10 @@ print(fastest_driver_2['LapTime'])
 telemetry_driver_1 = fastest_driver_1.get_telemetry().add_distance()
 telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
 
-driver_1_style = plotting.get_driver_style(driver_1, ['color', 'linestyle'], ff1.get_session(2024, "Japan", 'Q'))
-driver_2_style = plotting.get_driver_style(driver_2, ['color', 'linestyle'], ff1.get_session(2024, "Japan", 'Q'))
+# plotting.setup_mpl()
+
+driver_1_style = plotting.driver_color(driver_1)
+driver_2_style = plotting.driver_color(driver_2)
 
 
 delta_time, ref_tel, compare_tel = utils.delta_time(fastest_driver_1, fastest_driver_2)
@@ -49,33 +51,33 @@ fig, ax = plt.subplots(7, gridspec_kw={'height_ratios': plot_ratios})
 ax[0].title.set_text(plot_title)
 
 
-ax[0].plot(ref_tel['Distance'], delta_time, **driver_2_style)
-ax[0].axhline(0, **driver_1_style)
+ax[0].plot(ref_tel['Distance'], delta_time, driver_2_style)
+ax[0].axhline(0, color=driver_1_style)
 ax[0].set(ylabel=f"Gap to {driver_1} (s)")
 
-ax[1].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], **driver_1_style, label=driver_1)
-ax[1].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], **driver_2_style, label=driver_2)
+ax[1].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], driver_1_style, label=driver_1)
+ax[1].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], driver_2_style, label=driver_2)
 ax[1].set(ylabel='Speed')
 ax[1].legend(loc="lower right")
 
-ax[2].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Throttle'], **driver_1_style, label=driver_1)
-ax[2].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Throttle'], **driver_2_style, label=driver_2)
+ax[2].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Throttle'], driver_1_style, label=driver_1)
+ax[2].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Throttle'], driver_2_style, label=driver_2)
 ax[2].set(ylabel='Throttle')
 
-ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Brake'], **driver_1_style, label=driver_1)
-ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Brake'], **driver_2_style, label=driver_2)
+ax[3].plot(telemetry_driver_1['Distance'], telemetry_driver_1['Brake'], driver_1_style, label=driver_1)
+ax[3].plot(telemetry_driver_2['Distance'], telemetry_driver_2['Brake'], driver_2_style, label=driver_2)
 ax[3].set(ylabel='Brake')
 
-ax[4].plot(telemetry_driver_1['Distance'], telemetry_driver_1['nGear'], **driver_1_style, label=driver_1)
-ax[4].plot(telemetry_driver_2['Distance'], telemetry_driver_2['nGear'], **driver_2_style, label=driver_2)
+ax[4].plot(telemetry_driver_1['Distance'], telemetry_driver_1['nGear'], driver_1_style, label=driver_1)
+ax[4].plot(telemetry_driver_2['Distance'], telemetry_driver_2['nGear'], driver_2_style, label=driver_2)
 ax[4].set(ylabel='Gear')
 
-ax[5].plot(telemetry_driver_1['Distance'], telemetry_driver_1['RPM'], **driver_1_style, label=driver_1)
-ax[5].plot(telemetry_driver_2['Distance'], telemetry_driver_2['RPM'], **driver_2_style, label=driver_2)
+ax[5].plot(telemetry_driver_1['Distance'], telemetry_driver_1['RPM'], driver_1_style, label=driver_1)
+ax[5].plot(telemetry_driver_2['Distance'], telemetry_driver_2['RPM'], driver_2_style, label=driver_2)
 ax[5].set(ylabel='RPM')
 
-ax[6].plot(telemetry_driver_1['Distance'], telemetry_driver_1['DRS'], **driver_1_style, label=driver_1)
-ax[6].plot(telemetry_driver_2['Distance'], telemetry_driver_2['DRS'], **driver_2_style, label=driver_2)
+ax[6].plot(telemetry_driver_1['Distance'], telemetry_driver_1['DRS'], driver_1_style, label=driver_1)
+ax[6].plot(telemetry_driver_2['Distance'], telemetry_driver_2['DRS'], driver_2_style, label=driver_2)
 ax[6].set(ylabel='DRS')
 ax[6].set(xlabel='Lap distance (meters)')
 
