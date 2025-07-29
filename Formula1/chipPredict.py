163a2d61ca95fc83827925278e46feddcf6073c9
Alby Joseph <albysoy1@gmail.com>
2025-07-29 14:26:34 -0500

    created cache to limit API calls

--- Formula1/chipPredict.py
+++ Formula1/chipPredict.py
@@ -1,13 +1,20 @@
 from urllib.request import urlopen
 import json
 import pandas as pd
+import time
+from pathlib import Path
 # import fastf1 as ff1
 
 # ff1.Cache.enable_cache('Formula1/cache')
 
 # Ergast API is deprecated, using manual get_driver_standings function instead
-def get_driver_standings():
-    driver_call = urlopen(f"https://api.openf1.org/v1/drivers?session_key=9693")
+def get_driver_standings(year=2025):
+    schedule_call = urlopen(f"https://api.openf1.org/v1/sessions?date_start>={year}-01-01&date_end<={year}-12-31&session_type=Race")
+    data = json.loads(schedule_call.read().decode('utf-8'))
+    schedule = pd.DataFrame(data)
+    session_keys = schedule['session_key'].tolist()
+    
+    driver_call = urlopen(f"https://api.openf1.org/v1/drivers?session_key={session_keys[0]}")
     driver_data = json.loads(driver_call.read().decode('utf-8'))
     drivers = pd.DataFrame(driver_data)
     standings = pd.DataFrame({
@@ -17,17 +24,20 @@ def get_driver_standings():
         'Team': drivers['team_name'],
     })
     standings.index = range(1, len(standings) + 1)
-    
-    schedule_call = urlopen(f"https://api.openf1.org/v1/sessions?date_start>=2025-01-01&session_type=Race")
-    data = json.loads(schedule_call.read().decode('utf-8'))
-    schedule = pd.DataFrame(data)
-    session_keys = schedule['session_key'].tolist()
 
     for key in session_keys:
-        session_call = urlopen(f"https://api.openf1.org/v1/session_result?session_key={key}")
-        session_data = json.loads(session_call.read().decode('utf-8'))
+        file_path = Path(f"Formula1/cache/openF1/session_{key}.json")
+        if file_path.exists():
+            with open(file_path, "r") as f:
+                session_data = json.load(f)
+        else:
+            session_call = urlopen(f"https://api.openf1.org/v1/session_result?session_key={key}")
+            session_data = json.loads(session_call.read().decode('utf-8'))
+            with open(file_path, "w") as f:
+                json.dump(session_data, f)
+            time.sleep(0.5)  # To avoid hitting API rate limits
+
         results = pd.DataFrame(session_data)
-        
         for index, row in results.iterrows():
             driver_number = row['driver_number']
             points = row['points']
