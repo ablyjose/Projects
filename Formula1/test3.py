fac952e77636a2d1068b9042b41d3a0b92f83816
Alby Joseph <albysoy1@gmail.com>
2025-08-04 20:27:37 -0500

    converted into weather_data testing grounds

--- Formula1/test3.py
+++ Formula1/test3.py
@@ -8,7 +8,22 @@ import pandas as pd
 
 ff1.Cache.enable_cache('Formula1/cache')
 
-year, gp, session = 2024, "Brazil", 'R' 
+driver = "NOR"
 
-race = ff1.get_session(year, gp, session)
-race.load(messages=True)
+year, gp, session = 2025, "Hungary", 'FP1' 
+
+p1 = ff1.get_session(year, gp, session)
+p1.load(weather=True, laps=True, telemetry=False, messages=False)
+
+session = "FP2"
+p2 = ff1.get_session(year, gp, session)
+p2.load(weather=True, laps=True, telemetry=False, messages=False)
+
+print(p1.laps.pick_drivers(driver).sort_values(by='Time')[['Time', 'LapTime', 'LapNumber', 'Stint', 'Compound', 'TyreLife']])
+print("Average Track Temp:", p1.weather_data['TrackTemp'].mean())
+print("Average Air Temp:", p1.weather_data['AirTemp'].mean())
+print()
+
+print(p2.laps.pick_drivers(driver).sort_values(by='Time')[['Time', 'LapTime', 'LapNumber', 'Stint', 'Compound', 'TyreLife']])
+print("Average Track Temp:", p2.weather_data['TrackTemp'].mean())
+print("Average Air Temp:", p2.weather_data['AirTemp'].mean())
