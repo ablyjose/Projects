8c2b06f601720fa547c9b77604c2c070f800176c
Alby Joseph <albysoy1@gmail.com>
2025-11-12 12:55:20 -0600

    removed Laps conversion

--- Formula1/listDriverRace.py
+++ Formula1/listDriverRace.py
@@ -5,21 +5,21 @@ from fastf1.core import Laps
 
 ff1.Cache.enable_cache('Formula1/cache')
 
-year, gp, session = 2025, "Hungary", 'Q'
+year, gp, type = 2025, "Belgium", 'R'
 
-quali = ff1.get_session(year, gp, session)
-quali.load()
+session = ff1.get_session(year, gp, type)
+session.load()
 
-drivers = ['LEC', 'HAM']
-list_final_laps = list()
+drivers = ['NOR', 'PIA']
 
 for driver in drivers:
-    driver_laps = quali.laps.pick_drivers(driver).pick_fastest()    
-    list_final_laps.append(driver_laps)
-
-final_laps = Laps(list_final_laps) \
-    .sort_values(by=['TyreLife']) \
-    .reset_index(drop=True)
+    driver_laps = session.laps.pick_drivers(driver)
+    pd.set_option('display.max_rows', None) 
+    # pd.set_option('display.max_columns', None)
+    print(driver_laps[['Driver', 'LapTime', 'LapNumber', 'Stint', 'Compound', 'TyreLife', 'Sector1Time', 'Sector2Time', 'Sector3Time', 'SpeedI1', 'SpeedI2', 'SpeedFL', 'SpeedST', 'Deleted', 'DeletedReason']]\
+          .sort_values(by='LapNumber')\
+            .reset_index(drop=True))
+    print("--------------------------------------------------------------------------------------------------------------------------------\n")
+    
 
 # print(final_laps.columns)
-print(final_laps[['Driver', 'LapTime', 'LapNumber', 'Stint', 'Compound', 'TyreLife', 'SpeedI1', 'SpeedI2', 'SpeedFL', 'SpeedST']])
