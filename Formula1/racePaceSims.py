6b768d84f7cb27d1219e5a71f1546e2ad6ccfbbd
Alby Joseph <albysoy1@gmail.com>
2025-11-12 13:45:28 -0600

    Had to hard code drivers in + updated for Brazil

--- Formula1/racePaceSims.py
+++ Formula1/racePaceSims.py
@@ -11,13 +11,13 @@ import pandas as pd
 
 ff1.Cache.enable_cache('Formula1/cache')
 
-year, gp, session_name = 2025, "Mexico", 'FP3'
+year, gp, session_name = 2025, "Brazil", 'R'
 
 session = ff1.get_session(year, gp, session_name)
 session.load(messages=False, weather=False)
 
 # pick the first four drivers in the order they appear in the loaded laps
-drivers = list(session.laps['Driver'].unique())[:4]
+drivers = ['NOR', 'PIA', 'VER'] # hard coded drivers in due to inconsistent order in which drivers appear in laps data
 linestyles = ['-', ':']
 linestyle_mapping = {driver: linestyles[i % len(linestyles)] for i, driver in enumerate(drivers)}
 race_sims = list()
