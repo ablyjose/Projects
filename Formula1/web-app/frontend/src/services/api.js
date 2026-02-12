e151c4036ee275b53899ff002edb71f600211713
Alby Joseph <albysoy1@gmail.com>
2026-02-12 14:35:53 -0600

    added longer timeout to account for non-cached FF1 data

--- Formula1/web-app/frontend/src/services/api.js
+++ Formula1/web-app/frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'http://localhost:8000',
-    timeout: 10000,
+    timeout: 60000, // 1 minute to account for non-cached data
 });
 
 export const getEvents = async (year = 2025) => {
