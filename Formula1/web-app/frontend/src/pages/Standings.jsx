42b2235ba033caacb99a9f5f3b46bbe52c4be94c
Alby Joseph <albysoy1@gmail.com>
2026-04-28 15:38:37 -0500

    solved issue caused by cancelled races in 2026

--- Formula1/web-app/frontend/src/pages/Standings.jsx
+++ Formula1/web-app/frontend/src/pages/Standings.jsx
@@ -7,7 +7,7 @@ const Standings = () => {
     const [standings, setStandings] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    const [year, setYear] = useState(2025);
+    const [year, setYear] = useState(2026);
 
     useEffect(() => {
         const fetchStandings = async () => {
